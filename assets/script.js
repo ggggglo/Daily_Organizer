5a54b66d0ded6ee3220f1e6342dd40990548171c
Gloriana Gonzalez <glogonzalez@Glorianas-MBP.lan>
2022-08-30 00:15:11 -0500

    added event listener

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,15 @@
 var save = document.querySelector(".saveBtn");
+var text = document.querySelector("textarea")
 
 var today = moment();
 $("#currentDay").text(today.format("LLLL"));
 
 save.addEventListener('click', function(){
-    window.prompt("Hello");
+    var textContent = text.value;
+    localStorage.setItem("eventStored", textContent);
+    text = localStorage.getItem("eventStored");
+    console.log(text);
 });
 
+console.log(text.content);
+console.log(save);
