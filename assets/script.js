248ce5f761e621f6dc21c113b013be76c94cc132
Gloriana Gonzalez <glogonzalez@Glorianas-MBP.lan>
2022-08-30 23:34:22 -0500

    created variables for local storage

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,64 @@
-var save = document.querySelector(".saveBtn");
-var text = document.querySelector("textarea");
+var save1 = document.querySelector("#save1");
+var save2 = document.querySelector("#save2");
+var save3 = document.querySelector(".save3");
+var save4 = document.querySelector(".save4");
+var save5 = document.querySelector(".save5");
+var save6 = document.querySelector(".save6");
+var save7 = document.querySelector(".save7");
+var save8 = document.querySelector(".save8");
+var save9 = document.querySelector(".save9");
+
+
+var text1 = document.querySelector("#text1");
+var text2 = document.querySelector("#text2");
+var text3 = document.querySelector("#text3");
+var text4 = document.querySelector("#text4");
+var text5 = document.querySelector("#text5");
+var text6 = document.querySelector("#text6");
+var text7 = document.querySelector("#text7");
+var text8 = document.querySelector("#text8");
+var text8 = document.querySelector("#text9");
+
+
 var hours = document.querySelector(".hour")
 
 var today = moment();
 $("#currentDay").text(today.format("LLLL"));
 
-save.addEventListener('click', function(){
-    var textContent = text.value;
-    localStorage.setItem("eventStored", textContent);
+
+save1.addEventListener('click', function(){
+    var textContent1 = text1.value;
+    localStorage.setItem("stored1", textContent1);
 });
+
+save2.addEventListener('click', function(){
+    var textContent2 = text2.value;
+    localStorage.setItem("stored2", textContent2);
+});
+
+save3.addEventListener('click', function(){
+    var textContent3 = text3.value;
+    localStorage.setItem("stored3", textContent3);
+});
+
+save4.addEventListener('click', function(){
+    var textContent4 = text4.value;
+    localStorage.setItem("stored4", textContent4);
+});
+
+
+
+
+$("#text1").val(localStorage.getItem("stored1"));
+$("#text2").val(localStorage.getItem("stored2"));
+$("#text3").val(localStorage.getItem("stored3"));
+$("#text4").val(localStorage.getItem("stored4"));
+$("#text5").val(localStorage.getItem("stored5"));
+$("#text6").val(localStorage.getItem("stored6"));
+$("#text7").val(localStorage.getItem("stored7"));
+$("#text8").val(localStorage.getItem("stored8"));
+$("#text9").val(localStorage.getItem("stored9"));
+
 
 var timeNow = moment().format('HH');
 
@@ -120,6 +170,8 @@ function hour17(){
     }  
 };
 
+
+console.log(timeNow);
 hour9();
 hour10();
 hour11();
@@ -129,5 +181,3 @@ hour14();
 hour15();
 hour16();
 hour17();
-
-console.log(timeNow);
